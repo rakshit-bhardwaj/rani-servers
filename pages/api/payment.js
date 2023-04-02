4f4c2f16f048a50a368dacc89824d9a64421929c
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2023-04-02 21:06:01 +0530

    Update payment.js

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -1,20 +1,21 @@
 const stripe = require('stripe')(process.env.STRIPE_TEST_SK_KEY);
-import { initializeApp } from "firebase/app";
-import { getFirestore,collection, addDoc } from 'firebase/firestore';
+// import { initializeApp } from "firebase/app";
+// import { getFirestore,collection, addDoc } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: "rani-servers.firebaseapp.com",
-  projectId: "rani-servers",
-  storageBucket: "rani-servers.appspot.com",
-  messagingSenderId: "937118940101",
-  appId: process.env.FIREBASE_APPID,
-  measurementId: "G-BG8X076EJM"
-};
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-const dbi = collection(db, 'steamIDs');
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_KEY,
+//   authDomain: "rani-servers.firebaseapp.com",
+//   projectId: "rani-servers",
+//   storageBucket: "rani-servers.appspot.com",
+//   messagingSenderId: "937118940101",
+//   appId: process.env.FIREBASE_APPID,
+//   measurementId: "G-BG8X076EJM"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app)
+// const dbi = collection(db, 'steamIDs');
 
 export default async function handler(req, res) {
 
@@ -26,7 +27,6 @@ export default async function handler(req, res) {
         const session = await stripe.checkout.sessions.create({
           line_items: [
             {
-              //test price: 'price_1Mpa97SJau31s6BFZmzJ3OBM',
               // live price: 'price_1MpcMCSJau31s6BFwafyOZuD',
               price: 'price_1Mpa97SJau31s6BFZmzJ3OBM',
               quantity: 1,
@@ -34,14 +34,14 @@ export default async function handler(req, res) {
           ],
           payment_method_types: ['card'],
           mode: 'payment',
-          success_url: `${req.headers.origin}/?success=true`,
+          success_url: `${req.headers.origin}/?success=true&rsteamid=${steamID}`,
           cancel_url: `${req.headers.origin}/?canceled=true`,
         });
         
         res.redirect(303, session.url);
-        addDoc(dbi, {
-          steamID: steamID
-       })
+      //   addDoc(dbi, {
+      //     steamID: steamID
+      //  })
       } catch (err) {
         res.status(err.statusCode || 500).json(err.message);
       }
